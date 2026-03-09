b512b92dcd3c51988256703080bbd241a236506e
guest-persil <fharadilla24si@mahasiswa.pcr.ac.id>
2026-03-09 15:35:28 +0700

    Pertemuan 2

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,44 @@
+export default function HelloWorld(){
+const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <GreetingBinjai/>
+            <GreetingBinjai/>
+            <GreetingBinjai/>
+            <GreetingBinjai/>
+            <UserCard 
+	            nama="Payaaa" 
+	            nim="2457301055"
+	            tanggal={new Date().toLocaleDateString()}
+	          />
+              <UserCard {...propsUserCard}/>
+        </div>
+    )
+}
+
+
+function GreetingBinjai(){
+    return (
+        <small> Salam Dari Binjai 😘</small>
+    )
+}
+
+function UserCard(props){
+    return (
+        <div>
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
+
---
+++ src/pertemuan-2/main.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld";
+import QuoteText from "./QuoteText";
+import Container from "./Container";
+import './custom.css';
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+
+            <Container>
+                <img src="img/bukber.webp" width= "100%"/>
+                 <HelloWorld/>
+                <QuoteText/>
+            </Container>
+           
+        </div>
+    )
