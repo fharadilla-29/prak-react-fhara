cb6c316e2d3e54a36dd712b165ad074aa35035ce
ajik676 <hirzi24si@mahasiswa.pcr.ac.id>
2026-04-27 16:21:56 +0700

    Pertemuan 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import Orders from "./pages/Orders";
-import NotFound from "./pages/NotFound";
-import ErrorPage from "./pages/ErrorPage";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Loading from "./components/Loading";
+
+const Dashboard = React.lazy(() => import("./pages/Main/Dashboard"));
+const Customers = React.lazy(() => import("./pages/Main/Customers"));
+const Orders = React.lazy(() => import("./pages/Main/Orders"));
+const NotFound = React.lazy(() => import("./pages/main/NotFound"));
+const ErrorPage = React.lazy(() => import("./pages/main/ErrorPage"));
+const MainLayout = React.lazy(() => import("./layouts/MainLayout")); 
+
+
+
 
 export default function App() {
   return (
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-        <div id="layout-wrapper" className="flex flex-row flex-1">
-            <Sidebar/>
-            <div id="main-content" className="flex-1 p-4">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/orders" element={<Orders />} />
-                    <Route path="/customers" element={<Customers />} />
-                    
-                    {/* Error Routes */}
-                    <Route path="/error-400" element={<ErrorPage code="400" title="Bad Request" description="Permintaan tidak dapat diproses oleh server." image="https://illustrations.popsy.co/gray/falling.svg" />} />
-                    <Route path="/error-401" element={<ErrorPage code="401" title="Unauthorized" description="Anda harus login terlebih dahulu." image="https://illustrations.popsy.co/gray/shaking-hands.svg" />} />
-                    <Route path="/error-403" element={<ErrorPage code="403" title="Forbidden" description="Anda tidak memiliki akses ke halaman ini." image="https://illustrations.popsy.co/gray/stop.svg" />} />
-                    
-                    {/* 404 Not Found */}
-                    <Route path="*" element={<ErrorPage code="404" title="Page Not Found" description="Halaman yang anda cari raib entah kemana." />} />
-                </Routes>
-            </div>
-        </div>
-    </div>
+    
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        {/* Rute Utama (Membutuhkan MainLayout) */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+
+          {/* Error Routes */}
+          <Route
+            path="/error-400"
+            element={
+              <ErrorPage
+                code="400"
+                title="Bad Request"
+                description="Permintaan tidak dapat diproses oleh server."
+                image="https://illustrations.popsy.co/gray/falling.svg"
+              />
+            }
+          />
+          <Route
+            path="/error-401"
+            element={
+              <ErrorPage
+                code="401"
+                title="Unauthorized"
+                description="Anda harus login terlebih dahulu."
+                image="https://illustrations.popsy.co/gray/shaking-hands.svg"
+              />
+            }
+          />
+          <Route
+            path="/error-403"
+            element={
+              <ErrorPage
+                code="403"
+                title="Forbidden"
+                description="Anda tidak memiliki akses ke halaman ini."
+                image="https://illustrations.popsy.co/gray/stop.svg"
+              />
+            }
+          />
+
+          {/* 404 Not Found */}
+          <Route
+            path="*"
+            element={
+              <ErrorPage
+                code="404"
+                title="Page Not Found"
+                description="Halaman yang anda cari raib entah kemana."
+              />
+            }
+          />
+        </Route>
+
+        {/* Rute Autentikasi (Membutuhkan AuthLayout) */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
